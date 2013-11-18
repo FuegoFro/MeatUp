a037de8b7b270a4aa584b9b0ef008bcc6a57ac75
Danny Weinberg <FuegoFro@gmail.com>
2013-11-18 12:16:18 -0800

    Make adding friends list more clear, with better icons and background color as feedback.

--- src/com/cs160/fall13/MeatUp/CheckableLinearLayout.java
+++ src/com/cs160/fall13/MeatUp/CheckableLinearLayout.java
@@ -1,53 +1,170 @@
 package com.cs160.fall13.MeatUp;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Checkable;
-import android.widget.CheckedTextView;
 import android.widget.LinearLayout;
 
-/*
- * This class is useful for using inside of ListView that needs to have checkable items.
- * Taken from http://tokudu.com/post/50023900640/android-checkable-linear-layout
- */
+// Taken from https://gist.github.com/christopherperry/3746480
 public class CheckableLinearLayout extends LinearLayout implements Checkable {
-    private CheckedTextView checkbox;
+
+    private boolean mChecked;
+
+    private static final int[] CHECKED_STATE_SET = {
+            android.R.attr.state_checked
+    };
+
+    public CheckableLinearLayout(Context context) {
+        this(context, null);
+        init();
+    }
 
     public CheckableLinearLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
+    }
+
+    private void init() {
+//        setClickable(true);
+    }
+
+    /**********************/
+    /**   Handle clicks  **/
+    /**********************/
+
+    @Override
+    public boolean performClick() {
+        toggle();
+        return super.performClick();
     }
 
     @Override
-    protected void onFinishInflate() {
-        super.onFinishInflate();
-        // find checked text view
-        int childCount = getChildCount();
-        for (int i = 0; i < childCount; ++i) {
-            View v = getChildAt(i);
-            if (v instanceof CheckedTextView) {
-                checkbox = (CheckedTextView) v;
-                break;
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return onTouchEvent(ev);
+    }
+
+    /**************************/
+    /**      Checkable       **/
+    /**************************/
+
+    public void toggle() {
+        setChecked(!mChecked);
+    }
+
+    public boolean isChecked() {
+        return mChecked;
+    }
+
+    public void setChecked(boolean checked) {
+        if (mChecked != checked) {
+            mChecked = checked;
+            refreshDrawableState();
+            setCheckedRecursive(this, checked);
+        }
+    }
+
+    private void setCheckedRecursive(ViewGroup parent, boolean checked) {
+        int count = parent.getChildCount();
+        for(int i = 0; i < count; i++ ) {
+            View v = parent.getChildAt(i);
+            if(v instanceof Checkable) {
+                ((Checkable) v).setChecked(checked);
+            }
+
+            if(v instanceof ViewGroup) {
+                setCheckedRecursive((ViewGroup)v, checked);
             }
         }
     }
 
+    /**************************/
+    /**   Drawable States    **/
+    /**************************/
+
     @Override
-    public boolean isChecked() {
-        return checkbox != null && checkbox.isChecked();
+    protected int[] onCreateDrawableState(int extraSpace) {
+        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+        if (isChecked()) {
+            mergeDrawableStates(drawableState, CHECKED_STATE_SET);
+        }
+        return drawableState;
     }
 
     @Override
-    public void setChecked(boolean checked) {
-        if (checkbox != null) {
-            checkbox.setChecked(checked);
+    protected void drawableStateChanged() {
+        super.drawableStateChanged();
+
+        Drawable drawable = getBackground();
+        if (drawable != null) {
+            int[] myDrawableState = getDrawableState();
+            drawable.setState(myDrawableState);
+            invalidate();
         }
     }
 
-    @Override
-    public void toggle() {
-        if (checkbox != null) {
-            checkbox.toggle();
+    /**************************/
+    /**   State persistency  **/
+    /**************************/
+
+    static class SavedState extends BaseSavedState {
+        boolean checked;
+
+        SavedState(Parcelable superState) {
+            super(superState);
         }
+
+        private SavedState(Parcel in) {
+            super(in);
+            checked = (Boolean)in.readValue(null);
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeValue(checked);
+        }
+
+        @Override
+        public String toString() {
+            return "CheckableLinearLayout.SavedState{"
+                    + Integer.toHexString(System.identityHashCode(this))
+                    + " checked=" + checked + "}";
+        }
+
+        public static final Parcelable.Creator<SavedState> CREATOR
+                = new Parcelable.Creator<SavedState>() {
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
+    }
+
+    @Override
+    public Parcelable onSaveInstanceState() {
+        // Force our ancestor class to save its state
+        Parcelable superState = super.onSaveInstanceState();
+        SavedState ss = new SavedState(superState);
+
+        ss.checked = isChecked();
+        return ss;
+    }
+
+    @Override
+    public void onRestoreInstanceState(Parcelable state) {
+        SavedState ss = (SavedState) state;
+
+        super.onRestoreInstanceState(ss.getSuperState());
+        setChecked(ss.checked);
+        requestLayout();
     }
 }
