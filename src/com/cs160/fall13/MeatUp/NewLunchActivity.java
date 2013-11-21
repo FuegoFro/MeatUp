7116b958eb721c17181013fa1b82e967e8b5416a
avi <avipress@gmail.com>
2013-11-21 13:33:26 -0800

    a bit of formatting on new lunch page

--- src/com/cs160/fall13/MeatUp/NewLunchActivity.java
+++ src/com/cs160/fall13/MeatUp/NewLunchActivity.java
@@ -5,9 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.*;
 
 import java.util.ArrayList;
@@ -109,7 +107,4 @@ public class NewLunchActivity extends ActionBarActivity {
             return view;
         }
     }
-
-
-
 }
