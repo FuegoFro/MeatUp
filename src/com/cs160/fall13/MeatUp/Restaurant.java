025b7eacbeab486752f99594b2600db38cbb0002
Colorado Reed <colorado.j.reed@gmail.com>
2013-11-24 15:46:04 -0800

    fixed typo

--- src/com/cs160/fall13/MeatUp/Restaurant.java
+++ src/com/cs160/fall13/MeatUp/Restaurant.java
@@ -13,15 +13,15 @@ public class Restaurant {
     private double lat,
                    lon;
     private boolean isVegatarian;
-    private boolean isVegean;
+    private boolean isVegan;
     private int rating;
 
-    public Restaurant(String title, double lat, double lon, boolean vegatarian, boolean vegean, int rating) {
+    public Restaurant(String title, double lat, double lon, boolean vegatarian, boolean vegan, int rating) {
         this.title = title;
         this.lat = lat;
         this.lon = lon;
         isVegatarian = vegatarian;
-        isVegean = vegean;
+        isVegan = vegan;
         this.rating = rating;
     }
 
@@ -29,48 +29,47 @@ public class Restaurant {
         return title;
     }
 
-    public double getLat() {
-        return lat;
-    }
-
-    public double getLon() {
-        return lon;
-    }
-
-    public boolean isVegatarian() {
-        return isVegatarian;
-    }
-
-    public boolean isVegean() {
-        return isVegean;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public double getLat() {
+        return lat;
     }
 
     public void setLat(double lat) {
         this.lat = lat;
     }
 
+    public double getLon() {
+        return lon;
+    }
+
     public void setLon(double lon) {
         this.lon = lon;
+    }
+
+    public boolean isVegatarian() {
+        return isVegatarian;
     }
 
     public void setVegatarian(boolean vegatarian) {
         isVegatarian = vegatarian;
     }
 
-    public void setVegean(boolean vegean) {
-        isVegean = vegean;
+    public boolean isVegan() {
+        return isVegan;
+    }
+
+    public void setVegan(boolean vegan) {
+        isVegan = vegan;
+    }
+
+    public int getRating() {
+        return rating;
     }
 
     public void setRating(int rating) {
         this.rating = rating;
     }
-
 }
