032a04249f43dfea3b5cfaf7a6c1d25a1e4dd4b2
aviaviavi <avipress@gmail.com>
2013-12-10 16:07:01 -0800

    changed lunch to include id

--- src/com/cs160/fall13/MeatUp/Lunch.java
+++ src/com/cs160/fall13/MeatUp/Lunch.java
@@ -10,12 +10,19 @@ public class Lunch implements Parcelable {
     private Calendar time;
     private String location;
     private ArrayList<String> attendees = new ArrayList<String>();
+    private int id;
 
     public Lunch (Calendar time, String location) {
         this.time = time;
         this.location = location;
+        this.id = (int) Math.random() * Integer.MAX_VALUE;
     }
 
+    public Lunch (Calendar time, String location, int id) {
+        this.time = time;
+        this.location = location;
+        this.id = id;
+    }
     public Calendar getTime() {
         return time;
     }
@@ -28,7 +35,7 @@ public class Lunch implements Parcelable {
         return location;
     }
 
-    public void setLocation(String location) {
+    public void setLocation(String locahtion) {
         this.location = location;
     }
 
@@ -38,6 +45,10 @@ public class Lunch implements Parcelable {
 
     public void setAttendees(ArrayList<String> attendees) {
         this.attendees = attendees;
+    }
+
+    public int getId() {
+        return id;
     }
 
     @Override
@@ -62,11 +73,13 @@ public class Lunch implements Parcelable {
         parcel.writeString(location);
         parcel.writeStringList(attendees);
         parcel.writeSerializable(time);
+        parcel.writeInt(id);
     }
 
     private Lunch(Parcel in) {
         location = in.readString();
         in.readStringList(attendees);
         time = (Calendar) in.readSerializable();
+        id = in.readInt();
     }
 }
